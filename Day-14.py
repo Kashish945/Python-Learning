076163393a594b2d2e41766728b94e568ce57b1f
kashish <kashishpimpalshende@gmail.com>
2026-07-28 12:51:58 +0530

    day-14 file updated

--- Day-14.py
+++ Day-14.py
@@ -1,8 +1,10 @@
 ### OPPs Basic
 
 ## Reference Variable
+
 # A reference variable is a variable that refers to (points to) an object in memory.
 
+# Example :
 class Student:
     def __init__(self,name):
         self.name=name
@@ -19,6 +21,7 @@ print(s2) # obj is not deleted because s2 still refers to it
 
 # Pass by Object Reference means that the function receives a reference to the same object passed by the caller. If the object is mutable, changes to the object are visible outside the function. If the parameter is reassigned to a new object, only the local reference changes.
 
+# Example 1:
 class Customer:
     def __init__(self,name):
         self.name=name
@@ -37,7 +40,7 @@ greet(custobj)
 
 print(custobj.name)
 
-
+# Example 2:
 class Customer:
     def __init__(self,name,gender):
         self.name=name
@@ -58,9 +61,11 @@ print(new_cust.name)
 
 
 ## Collection of Object
-#A collection of objects means storing multiple objects of the same class (or different classes) inside a Python collection such as: List, Tuple, Set ,Dictionary
-#This allows us to process many objects together.
 
+    # A collection of objects means storing multiple objects of the same class (or different classes) inside a Python collection such as: List, Tuple, Set ,Dictionary
+    # This allows us to process many objects together.
+
+# Example 1:
 class Student:
     def __init__(self,name,age):
         self.name=name
@@ -78,7 +83,7 @@ for i in l:
     print(i.name,i.age)
     i.intro()
     
-# Creating object directly in list
+# Example 2: Creating object directly in list
 class Student:
     def __init__(self, name):
         self.name = name
@@ -97,6 +102,8 @@ for student in students:
 ## Static
 #A static variable (class variable) is a variable that is declared inside the class but outside any methods. It has only one copy, which is shared among all instances (objects) of the class.
 # Static varible are created outside the constructor whereas the instance variable are created inside the constructor
+
+# Example 1:
 class Student:
     college = "ABC College"    # Static (Class) Variable
     def __init__(self, name):
@@ -108,7 +115,7 @@ s2 = Student("Janhavi")
 print(s1.college)
 print(s2.college)
 
-# Changing Static Variable using class name
+# Example 2 : Changing Static Variable using class name
 class Student:
     college = "ABC College"
 
@@ -120,3 +127,28 @@ Student.college = "XYZ College"
 print(s1.college)
 print(s2.college)
 print(Student.college)
+
+
+## Static method
+
+# A static method is a method that belongs to a class but does not operate on instance (self) or class (cls) data. It behaves like a regular function but is grouped inside the class because it is logically related to that class.
+# To define a static method, use the @staticmethod decorator.
+
+# Example 1:
+class Math:
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+print(Math.add(10, 20))
+
+# Example 2: Calling Static Method Using an Object
+class Math:
+
+    @staticmethod
+    def square(n):
+        return n * n
+
+obj = Math()
+print(obj.square(5))
